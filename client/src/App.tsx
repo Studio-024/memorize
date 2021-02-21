eccb1f255be44c56437c314ec1eaf2c08d511e76
ViniciusGregorine <viniciusgmenegon@gmail.com>
2021-02-21 18:06:54 -0300

    setupping the react files, deleting the standard create-react-app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    	<header className="App-header">
+    		<p>esse é um reader</p>
+      	</header>
+		  
+		<article>
+			<p>esse é o artigo</p>
+		</article>
     </div>
   );
 }
