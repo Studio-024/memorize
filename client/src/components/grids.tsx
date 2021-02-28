2ab39a8340ab4b2ca08c3c45a953df2ff9accc3b
Alexandre Costa Belettini <61050276+AlexandreXYZ@users.noreply.github.com>
2021-02-28 02:20:17 -0300

    interface v1.0

--- client/src/components/grids.tsx
+++ client/src/components/grids.tsx
@@ -1,6 +1,38 @@
 import React from 'react';
 export default function grids(){
     return(
-    <div>a</div>
+    <div className="conteiner">
+
+        <div className="status">
+        
+            <div className="direita">
+                <div>Revisado: </div>
+            </div>
+        
+            <div className="esquerda">
+                <div>Falta: </div>  
+                <div>Total: </div>
+            </div>
+        
+        </div>
+        
+        <div className="cards">
+            <div className="quests"> 
+                <p className="text">Por que a visão da idade média do renascimento, não é igual do iluminismo, que não é igual ao da era industrial. mesmo sendo sobre o mesmo assunto?</p>
+            </div>
+            <div className="quests">
+                <p className="text">pois esses períodos são mais antigos portanto menos informados e "ignorantes".</p>
+            </div>
+            
+            <div className="acertos">
+                <div id="acerto">Acertei</div>
+                <div id="erro">Errei</div>
+            </div>
+        </div>
+        <div className="new">
+            <div className="three">c</div>
+        </div>
+
+    </div>
     );
 }
