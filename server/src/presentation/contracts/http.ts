3df7feb268f4ce4e705d8938de26e9834f83b322
ViniciusGregorine <viniciusgmenegon@gmail.com>
2021-03-02 13:45:23 -0300

    refactor: organizing the custom return function

--- server/src/presentation/contracts/http.ts
+++ server/src/presentation/contracts/http.ts
@@ -3,18 +3,35 @@ export type HttpResponse<T = any> = {
     data: T
   }
 
-export type HttpRequest = {
-  body?: any
+export type HttpRequest <T = any>= {
+  body?: T
 }
+
+export const ok = (data: any): HttpResponse => ({
+  statusCode: 200,
+  data
+})
+
+export const noContent = (): HttpResponse => ({
+  statusCode: 200,
+  data: null
+})
+
+export const genericError = (error: {message: string, status: number, name: string}) => ({
+  statusCode: error.status,
+  data: `${error.name}: ${error.message}` 
+})
   
   export const serverError = (error: Error): HttpResponse => ({
     statusCode: 500,
-    data: error.stack
+    data: error.message
   })
   
-  export const ok = (data: any): HttpResponse => ({
-    statusCode: 200,
-    data
+ 
+
+  export const badRequest = (error: any): HttpResponse => ({
+    statusCode: 400,
+    data: error
   })
 
   export const notFound = (data: string): HttpResponse => ({
