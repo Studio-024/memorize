a742a27402dd49e4ae722b1675e2fdb204706a31
ViniciusGregorine <viniciusgmenegon@gmail.com>
2021-03-02 13:52:11 -0300

    bug: adapter dont receiving the request

--- server/src/main/adapters/expressRouterAdapter.ts
+++ server/src/main/adapters/expressRouterAdapter.ts
@@ -4,7 +4,7 @@ import { Request, Response } from 'express'
 
 export const adaptRoute = (controller: IController) => {
   return async (req: Request, res: Response) => {
-    const httpResponse = await controller.handle()
+    const httpResponse = await controller.handle(req)
     res.status(httpResponse.statusCode).json(httpResponse.data)
   }
 }
