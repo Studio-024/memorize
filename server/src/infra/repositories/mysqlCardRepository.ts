245b342656caf6aa5b42fbee54b3892f65c428a5
ViniciusGregorine <viniciusgmenegon@gmail.com>
2021-03-02 13:54:06 -0300

    feat: adding feature on mysql repository

---
+++ server/src/infra/repositories/mysqlCardRepository.ts
@@ -0,0 +1,21 @@
+import { IAddCardRepository } from "@/data/contracts/addCardRepository";
+import { IListCardRepository } from "@/data/contracts/listCardRepository";
+import { ICardViewModel } from "@/data/models/cardViewModel";
+import { ICardMysqlViewModel } from "../models/cardMysqlViewModel";
+import { pool } from "../mysqlPoolConnection";
+
+export class MysqlcardRepository implements IListCardRepository, IAddCardRepository{
+    async listCard(): Promise<ICardMysqlViewModel[]>{  
+        try {
+            const [rows] =  await pool.query<ICardMysqlViewModel[]>( 'select * from flashcard', []); 
+            return rows
+        } catch (error) {
+            throw new Error(error)
+       }
+    }
+
+    async addCard(card: ICardViewModel): Promise<void>{
+        await pool.query(`INSERT INTO flashcard SET ?`, card);
+            
+    }
+}
