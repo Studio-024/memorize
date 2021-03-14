c6185eb8c823e5cf1844f0ece210b05c349a481a
k1vz <mrnerd777@gmail.com>
2021-03-14 18:47:23 -0300

    :construction: Created '.htacess' file

--- client/src/service/api-data.ts
+++ client/src/service/api-data.ts
@@ -1,12 +1,12 @@
 import React from 'react';
 import Axios from 'axios';
-import Cors from 'cors';
 
 export const getCard = async() => {
     try {
-        const {data} = await Axios.get('http://localhost:3001/card')
+        //const {data} = await Axios.get('http://localhost:3001/card')
         //Axios.get('http://localhost:3001/card')
-        console.log({data})
+        //console.log({data})
+        console.log("oi");
     }
     catch (error) {
         console.error(error);
