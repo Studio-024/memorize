4d02d661bda94b1f9b9d0c4530f257a3d78cffbb
k1vz <mrnerd777@gmail.com>
2021-03-15 20:14:05 -0300

    :construction: Starting the feature 'print array'
    Testing differents ways to print the content of array

--- client/src/service/api-data.ts
+++ client/src/service/api-data.ts
@@ -1,11 +1,14 @@
 import Axios from 'axios';
 
 export const getCard = async() => {
-    try {
-        const {data} = await Axios.get('http://localhost:3001/card')
-        console.log({data})
-    }
-    catch (error) {
-        console.error(error);
-    }
+	try {
+		const {data} = await Axios.get('http://localhost:3001/card')
+		
+		console.log({data}.data[1]);
+		console.log("{data}.data", {data}.data); // Retorna uma array 
+		//console.log("{data}", {data}) 		// Retorna um objeto
+	}
+	catch (error) {
+		console.error(error);
+	}
 }
