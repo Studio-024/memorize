4308641adbe0bc88e1f0197f0a655dca9b11019f
Alexandre Costa Belettini <61050276+AlexandreXYZ@users.noreply.github.com>
2021-03-20 18:32:32 -0300

    :bug: error merge corrected

--- client/src/dashboardComponents/Dashboard.tsx
+++ client/src/dashboardComponents/Dashboard.tsx
@@ -12,36 +12,13 @@ interface Props {
 export default function Dashboard({quest, response}: Props){ 
     return(
     <div className="conteiner">
-        <div className="status">
+        <Status/>
         
-            <div className="left">
-                <div>Revisado: </div>
-            </div>
-        
-            <div className="right">
-                <div>Falta: </div>  
-                <div>Total: </div>
-            </div>
-        
-        </div>
-        
-        <div className="cards">
-            <div className="quests"> 
-                <p className="text" id="print_question">{quest}</p>
-            </div>
-            <div className="quests">
-                <p className="text" id="print_response">{response}</p>
-            </div>
+        <Cards quest={quest} response={response} />
             
-            <div className="acertos">
-                <div id="acerto">Acertei</div>
-                <div id="erro">Errei</div>
-            </div>
-        </div>
-        <div className="new">
+        <New/>
             {/*Printa o cod*/}
             <div className="three" id="print_cod"></div>
-        </div>
     </div>
     );
 }
