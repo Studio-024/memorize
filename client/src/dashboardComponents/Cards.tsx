b07070ae2621dc48b67e9a3e8b6323b3abb26a66
Alexandre Costa Belettini <61050276+AlexandreXYZ@users.noreply.github.com>
2021-03-21 16:48:45 -0300

    restore

---
+++ client/src/dashboardComponents/Cards.tsx
@@ -0,0 +1,25 @@
+import React, { ReactNode } from 'react';
+interface Props {
+    quest: string;
+    response: string;
+}
+export default function Cards({quest, response}: Props){
+    return(
+        <div className="dashboard__content__cards">
+            <div className="dashboard__content__questFlex" id="dashboard__content__quest"> 
+                <p className="dashboard__content__text">{quest}</p>
+            </div>
+            
+            <div className="dashboard__content__questFlex" id="dashboard__content__response">
+                <div  className="dashboard__content__see" >
+                    <button className="buttons" id="dashboard__content__SeeResponse" >Resposta</button>    
+                </div>     
+            </div>
+            
+            <div className="dashboard__content__next">
+                <button className="buttons" id="dashboard__content__erro">Errei</button>
+                <button className="buttons" id="dashboard__content__acerto">Acertei</button>
+            </div>
+        </div>
+    )
+}
