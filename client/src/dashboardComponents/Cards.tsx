f822bb666ab13c17bb24bfac8f28ae56b9770209
ViniciusGregorine <viniciusgmenegon@gmail.com>
2021-03-22 20:36:36 -0300

    :construction: Add useState hook on component Cards

--- client/src/dashboardComponents/Cards.tsx
+++ client/src/dashboardComponents/Cards.tsx
@@ -1,13 +1,17 @@
-import React, { ReactNode } from 'react';
+import { useState } from 'react';
+
+
 interface Props {
     quest: string;
     response: string;
 }
 export default function Cards({quest, response}: Props){
+    const [count, setCount] = useState(0)
+
     return(
         <div className="dashboard__content__cards">
             <div className="dashboard__content__questFlex" id="dashboard__content__quest"> 
-                <p className="dashboard__content__text">{quest}</p>
+                <p className="dashboard__content__text">{count}</p>
             </div>
             
             <div className="dashboard__content__questFlex" id="dashboard__content__response">
@@ -17,7 +21,9 @@ export default function Cards({quest, response}: Props){
             </div>
             
             <div className="dashboard__content__next">
-                <button className="buttons" id="dashboard__content__erro">Errei</button>
+                <button className="buttons" id="dashboard__content__erro" onClick={()=>{
+                    setCount(count + 1)
+                }}>Errei</button>
                 <button className="buttons" id="dashboard__content__acerto">Acertei</button>
             </div>
         </div>
