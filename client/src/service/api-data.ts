d8eefb80bb63b621fb2e070aad3438ee7d4068b9
k1vz <mrnerd777@gmail.com>
2021-03-22 19:57:57 -0300

    :arrow_up: Added React-DOM
    Added React-DOM
    Changed some files to try to create the index ordener

--- client/src/service/api-data.ts
+++ client/src/service/api-data.ts
@@ -1,13 +1,16 @@
 import Axios from 'axios';
+import '../components/OrderCards';
+import Counter from '../components/OrderCards';
 
 export const getCard = async() => {
 	try {
 		const {data} = await Axios.get('http://localhost:3001/card')
 		
+		Counter();
+		console.log("Working");
 		document.getElementById('print_cod')!.innerHTML = {data}.data[0].cod;
-		document.getElementById('print_question')!.innerHTML = {data}.data[0].question;
-		document.getElementById('print_response')!.innerHTML =  {data}.data[0].response;
-
+		//document.getElementById('print_question')!.innerHTML = {data}.data[0].question;
+		//document.getElementById('print_response')!.innerHTML =  {data}.data[0].response;
 	}
 	catch (error) {
 		console.error(error);
