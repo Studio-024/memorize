0201f7cac30654b92ad14e01319f6972ded6af0a
ViniciusGregorine <viniciusgmenegon@gmail.com>
2021-03-23 10:36:06 -0300

    :construction: to apply ICard[] type in  API axios request

--- client/src/service/api-data.ts
+++ client/src/service/api-data.ts
@@ -1,15 +1,14 @@
 import Axios from 'axios';
+import { ICard } from '../domain/entities/Card';
 
 export const getCard = async() => {
 	try {
-		const {data} = await Axios.get('http://localhost:3001/card')
+		const {data} = await Axios.get<ICard[]>('http://localhost:3001/card')
 
-		return data;
-		// document.getElementById('print_cod')!.innerHTML = {data}.data[0].cod;
-		// document.getElementById('print_question')!.innerHTML = {data}.data[0].question;
-		// document.getElementById('print_response')!.innerHTML =  {data}.data[0].response;
+		return data
 	}
 	catch (error) {
-		console.error(error);
+		console.error(error)
+		throw new Error(error)
 	}
 }
