241d1ee6471757f55bbb30ff9e3ca60535fcc629
k1vz <mrnerd777@gmail.com>
2021-03-22 23:00:13 -0300

    :art: Removed some useless code
    Deleted 'components/OrderCards.tsx'
    Deleted 'orderCards.tsx'
    Solved merge conflict in 'service/api-data.ts'
    Removed useless imports in 'service/api-data.ts'

--- client/src/service/api-data.ts
+++ client/src/service/api-data.ts
@@ -1,25 +1,13 @@
 import Axios from 'axios';
-import '../components/OrderCards';
-import Counter from '../components/OrderCards';
 
 export const getCard = async() => {
 	try {
 		const {data} = await Axios.get('http://localhost:3001/card')
-		
-<<<<<<< HEAD
-		Counter();
-		console.log("Working");
-		document.getElementById('print_cod')!.innerHTML = {data}.data[0].cod;
-		//document.getElementById('print_question')!.innerHTML = {data}.data[0].question;
-		//document.getElementById('print_response')!.innerHTML =  {data}.data[0].response;
-=======
-		return data
-		
+
+		return data;
 		// document.getElementById('print_cod')!.innerHTML = {data}.data[0].cod;
 		// document.getElementById('print_question')!.innerHTML = {data}.data[0].question;
 		// document.getElementById('print_response')!.innerHTML =  {data}.data[0].response;
-
->>>>>>> main
 	}
 	catch (error) {
 		console.error(error);
