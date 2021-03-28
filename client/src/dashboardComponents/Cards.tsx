36f8f41871c88ad44ac126c600200812a324ab19
k1vz <mrnerd777@gmail.com>
2021-03-28 17:17:12 -0300

    :bug: Fixed index limitator and added documentations
    Check if 'card' exists before run it
    Remove 1 from 'index' to run correctly
    Added some documentation

--- client/src/dashboardComponents/Cards.tsx
+++ client/src/dashboardComponents/Cards.tsx
@@ -1,44 +1,41 @@
 import { useState, useEffect } from 'react';
-import { ICardOrdered } from '../domain/useCase/orderCard';
 import { getCard } from '../service/api-data';
+import { ICardOrdered } from '../domain/useCase/orderCard';
 import { OrderCardService } from '../service/OrderCardService';
 
-
 export default function Cards() {
-
-    const [card, setCard] = useState<ICardOrdered[]>([])
+    const [index, setIndex] = useState(0);
     const [question, setQuestion] = useState('');
     const [response, setResponse] = useState('');
-    const [index, setIndex] = useState(0);
+    const [card, setCard] = useState<ICardOrdered[]>([]);
 
-    useEffect(()=>{
-        async function execute(){
+    useEffect(() => {
+        // Get the cards from db in first render
+        async function execute() {
             const orderObj = new OrderCardService();
             const data = await orderObj.order(getCard());
-            console.log('pegando os cards')
-            setCard(data)
+            
+            setCard(data);
+            console.log("Getting cards");
         }
 
         execute()
     }, [])
 
-    useEffect(() => {
-            // Criar função do back-end para evitar request atoa
-            if (index < card.length) {
-                console.log('setquestion')
-                setQuestion(card[index].question);
-                setResponse(card[index].response);
-            }
-            else {
-                console.log('vai toma no cu')
-                setQuestion("Não há mais cards.");
-                setResponse("Não há mais cards.");
-            }
-        
+    useEffect(() => {     //Criar função do back-end para evitar request atoa
+        // Whenever 'index' has a modification, this content will be rendered
+        if (card.length && index <= card.length) {
+            // Check if 'card' exists and 'index' is smaller than 'card.length'
+            setQuestion(card[index - 1].question);
+            setResponse(card[index - 1].response);
+        }
+        else {
+            setQuestion("Não há mais cards.");
+            setResponse("Não há mais cards.");
+        }
     }, [index]);
 
-    
-    
+
     return(
         <>
         <section className="dashboard__content__cards">
@@ -48,8 +45,7 @@ export default function Cards() {
             </section>
             
             <section className="dashboard__content__questFlex" id="dashboard__content__response">
-                <div  className="dashboard__content__see" >
-                    
+                <div  className="dashboard__content__see" >                    
                     <button className="buttons" id="dashboard__content__SeeResponse" >resposta</button>    
                 </div>     
                 <p className="dashboard__content__flexText">{response}</p>
@@ -59,14 +55,12 @@ export default function Cards() {
 
         <section className="dashboard__content__next">
             <button className="buttons" id="dashboard__content__erro" onClick={() => {
-                console.log('clicado errado')
                 setIndex(index + 1)
             }}>Errei</button>
             <button className="buttons" id="dashboard__content__acerto" onClick={() => {
-                 console.log('clicado certo')
                 setIndex(index + 1)
             }}>Acertei</button>
         </section>
         </>
-    )
+    );
 }
