46c8352bc9985f147e21e683ef81e863dff4b564
Alexandre Costa Belettini <61050276+AlexandreXYZ@users.noreply.github.com>
2021-04-18 19:10:54 -0300

    :rocket: Feature complete
    At first there is only the answer button, which when clicked disappears and the answer appears.
    When you click on "Acertei" or "Errei" it appears again and erases the answer from the screen. - Google translate :trollface:

--- client/src/dashboardComponents/Cards.tsx
+++ client/src/dashboardComponents/Cards.tsx
@@ -1,32 +1,36 @@
-import { useState, useEffect } from 'react'
-import { ICardOrdered } from '../domain/useCase/orderCard'
+import { useState, useEffect } from 'react';
+import { ICardOrdered } from '../domain/useCase/orderCard';
 
-export interface Props {
+export interface Props{
     dataCards: ICardOrdered[]
     buttonIndex: number
 }
 
 export default function Cards({dataCards, buttonIndex}: Props) {
-    const [question, setQuestion] = useState('')
-    const [response, setResponse] = useState('')
-    const [card, setCard] = useState<ICardOrdered[]>([])
-
-    useEffect(() => {
+    const [question, setQuestion] = useState('');
+    const [response, setResponse] = useState('');
+    const [card, setCard] = useState<ICardOrdered[]>([]);
+    
+    useEffect(()=>{ 
         setCard(dataCards)
     }, [dataCards])
 
     useEffect(() => {
         if (card.length && buttonIndex <= card.length) {
-            setQuestion(card[buttonIndex - 1].question)
-            setResponse(card[buttonIndex - 1].response)
+            setQuestion(card[buttonIndex - 1].question);
+            setResponse(card[buttonIndex - 1].response);
         }
         else {
-            setQuestion("Não há mais cards.")
-            setResponse("Não há mais cards.")
+            setQuestion("Não há mais cards.");
+            setResponse("Não há mais cards.");
         }
-    }, [buttonIndex])
+    }, [buttonIndex]);
+    
+    function handleClick(){
+        document.getElementById("dashboard__content__SeeResponse")!.style.display = "none";
+        document.getElementById("resposta")!.style.display = "inline";
 
-
+    }
     return(
         <>
         <section className="dashboard__content__cards">
@@ -36,10 +40,10 @@ export default function Cards({dataCards, buttonIndex}: Props) {
             </section>
             
             <section className="dashboard__content__questFlex" id="dashboard__content__response">
+                <p className="dashboard__content__flexText" id="resposta" style={{display: "none"}}>{response} aa a a a a a a a a  a a a aa  a a a a a a a a a a a a a a a a a a a a a a a a a a a a a aa a a a a a a a a a a a a a a a a a a a a aa a a a a a a a a a a a a a a a a a a a a aa a a a a a a a a a a a a a a a a a a a a a </p>
                 <div  className="dashboard__content__see" >                    
-                    <button className="buttons" id="dashboard__content__SeeResponse" >resposta</button>    
+                    <button className="buttons" id="dashboard__content__SeeResponse" onClick={handleClick}>resposta</button>    
                 </div>     
-                <p className="dashboard__content__flexText">{response}</p>
             </section>
             
         </section>
