c183546f737cc84b18b201859e7f2a9adda2bfaa
k1vz <mrnerd777@gmail.com>
2021-06-15 23:01:24 -0300

    added 'axios method' (but not working corretly)

--- client/src/service/apiData.ts
+++ client/src/service/apiData.ts
@@ -13,3 +13,22 @@ export const getCard = async() => {
 		throw new Error(error);
 	}
 }
+
+export const saveCard = async(question:string, response:string) => {
+	try {
+		const res = await Axios.post<ICard[]>('http://localhost:3001/card', {
+			question: question,
+			response: response,
+			timeEnd: "2021-03-15T02:09:34.000Z",
+			timeNext: "2021-03-15T02:09:34.000Z"		
+		});
+
+		console.log(res);
+
+	}
+	
+	catch (error) {
+		console.error(error);
+		throw new Error(error);
+	}
+}
