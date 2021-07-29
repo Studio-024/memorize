054d76f747ea46f7c9882be01dc2855d533e08a3
Alexandre Costa Belettini <costaalexandre45@gmail.com>
2021-07-29 15:36:32 -0300

    test(client): add "history" from URL modify
    History is a library from "ReactTraining" repository that allows us modify the website URL
    
    BREAKING CHANGE: error in app.tsx because of a useEffect (comment to resolve)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,16 @@ import './css/App.css'
 import Dashboard from './pages/Dashboard'
 import {getCard} from './service/api'
 import AddCard from './pages/AddCard'
+import { useEffect, useState } from 'react'
+
+
 export default function App() {
+	const [getDashbortType, setDashboardType] = useState('Dashboard')
+	useEffect(() => {
+		history.pushState('test','test','/test');
+		if(getDashbortType == 'Dashboard'){
+		}
+	})
 	return(
 		<>
 			<Dashboard/>
