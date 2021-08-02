0d73658e010c9f2038d1cbf027ead1d7fc2d38b7
k1vz <mrnerd777@gmail.com>
2021-08-02 15:32:08 -0300

    refactor(client): remove useless code in 'AddCard.tsx'
    Removed 'download cards' feature from 'AddCard.tsx', because it was useless.

--- client/src/pages/AddCard.tsx
+++ client/src/pages/AddCard.tsx
@@ -1,32 +1,16 @@
-import { getCard } from '../service/api'
-import { useEffect, useState} from 'react'
+import { useState} from 'react'
 import React from 'react'
-import { ICardOrdered } from '../domain/useCase/orderCard'
-import { OrderCardService } from '../service/OrderCardService'
 import AddQuest  from '../components/AddQuest'
 import AddResponse  from '../components/AddResponse'
 const CardContext = React.createContext('');
 
-const AddCard = () => { 
-    const [orderCards, setOrderCards ] = useState<ICardOrdered[]>([])
+const AddCard = () => {
     const [index, setIndex] = useState(0)
-    
-    useEffect(() => {
-        async function downloadCards() {
-            const orderObj = new OrderCardService()
-            const data = await orderObj.order(getCard())
 
-            setOrderCards(data)
-        }
-
-        downloadCards()
-
-    }, [])
     function resetCard() {
         setIndex(index + 1)
         document.getElementById("dashboard__content__back")!.style.display = "none"
         document.getElementById("dashboard__content__seeBack")!.style.display = "initial"
-        
     }
 
 	return(
