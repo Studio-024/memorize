b4d862f5bbf7bd1777572cf532aa668f74d66e7d
ViniciusGregorine <viniciusgmenegon@gmail.com>
2021-08-04 15:48:14 -0300

    feat(server): add `IReview` entity and `IRviewCard` use case

---
+++ server/src/domain/entities/review.ts
@@ -0,0 +1,10 @@
+export interface IReview {
+    cod: number
+    userGrade: number
+    
+    streak: number
+    interval_time: number
+    easiness_factor?: number
+
+    revison_date?: Date
+}
--- server/src/domain/usecases/ReviewCard.ts
+++ server/src/domain/usecases/ReviewCard.ts
@@ -1,10 +1,5 @@
-interface IReview {
-    streak: number
-    interval_time: number
-    revison_date: Date
-    userGrade: number
-    easinessFactor: number
-}
+import { IReview } from "../entities/review";
+
 
 export interface IReviewCard{ 
     reviewCard(card: IReview): Promise<void>
