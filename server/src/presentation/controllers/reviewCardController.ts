964c8ba46186a29276aa77df16a46d1a8cd881c4
ViniciusGregorine <viniciusgmenegon@gmail.com>
2021-08-05 17:26:59 -0300

    feat: add `reviewCardUseCase` on presentation layer

---
+++ server/src/presentation/controllers/reviewCardController.ts
@@ -0,0 +1,33 @@
+
+import { IReview } from "@/domain/entities/review";
+import { IReviewCardUseCase } from "@/domain/usecases/reviewCard";
+import { IController } from "../contracts/controller";
+import { errorHandler, HttpRequest, HttpResponse, noContent } from "../contracts/http";
+
+export class reviewCardController implements IController {
+    constructor(
+        private readonly reviewCardUseCase: IReviewCardUseCase 
+    ){}
+
+
+    // add infra viewModel
+    async handle(request: HttpRequest<IReview>): Promise<HttpResponse>{
+        const ReviewCard: IReview = {
+            cod: request.body!.cod,
+            streak : request.body!.streak,
+            interval_time: request.body!.interval_time,
+            userGrade: request.body!.userGrade
+        }
+
+
+        try {
+           await this.reviewCardUseCase.reviewCard(ReviewCard)
+           return noContent()
+
+        } catch (error) {
+           return errorHandler(error.response) 
+        }
+
+
+    }
+}
