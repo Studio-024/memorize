16b43d28c7978d9a8f9053df5621cb600e132013
Alexandre Costa Belettini <costaalexandre45@gmail.com>
2021-08-08 17:40:58 -0300

    fix(client): fix in package.json and Dashboard.tsx
    BREAKING CHANGE: imports bug in addcard.tsx

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,20 +1,19 @@
 import React from 'react'
 import { useEffect, useState} from 'react'
-import AddCard  from '../components/AddQuest'
-import RevisionQuest from '../components/RevisionQuest'
-import RevisionResponse from '../components/RevisionResponse'
+import RevisionFront from '../components/Revision'
+import RevisionBack from '../components/Revision'
 import { getCard } from '../service/api'
 import { ICardOrdered } from '../domain/useCase/orderCard'
 import { OrderCardService } from '../service/OrderCardService'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.min.css'
+import { Route, Switch } from 'react-router-dom'
 
 const CardContext = React.createContext('');
 
 const Dashboard = () => { 
     const [orderCards, setOrderCards ] = useState<ICardOrdered[]>([])
     const [index, setIndex] = useState(0)
-    let match = useRouteMatch();
     useEffect(() => {
         async function downloadCards() {
             const orderObj = new OrderCardService()
