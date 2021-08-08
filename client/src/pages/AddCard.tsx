808e986d41ffd5cfff3bf990a859cb27dc27d3ff
Pedro Duarte <61050418+k1vz@users.noreply.github.com>
2021-08-07 23:19:03 -0300

    Update AddCard.tsx

--- client/src/pages/AddCard.tsx
+++ client/src/pages/AddCard.tsx
@@ -1,5 +1,4 @@
-import { useState} from 'react'
-import React from 'react'
+import React, { useState } from 'react'
 import AddQuest  from '../components/AddQuest'
 import AddResponse  from '../components/AddResponse'
 const CardContext = React.createContext('');
