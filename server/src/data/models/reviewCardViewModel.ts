0f3e31e795dfccee09681bec38d797346f2b6f2e
ViniciusGregorine <viniciusgmenegon@gmail.com>
2021-08-09 13:36:06 -0300

    refactor(server): update viewlModels for presentetion and Data layer

--- server/src/data/models/reviewCardViewModel.ts
+++ server/src/data/models/reviewCardViewModel.ts
@@ -1,4 +1,8 @@
 
 import { IReview } from '@/domain/entities/review';
 
-export interface IReviewCardViewModel extends IReview{}
+export interface IReviewCardViewModel extends IReview{
+    interval_time: number
+    easiness_factor: number
+    streak: number
+}
--- server/src/domain/entities/review.ts
+++ server/src/domain/entities/review.ts
@@ -2,8 +2,8 @@ export interface IReview {
     cod: number
     userGrade: number
     
-    streak: number
-    interval_time: number
-    easiness_factor: number
+    streak?: number
+    interval_time?: number
+    easiness_factor?: number
 
 }
