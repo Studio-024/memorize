466468982b316e55d4bfb1e09a8ac86e954de005
ViniciusGregorine <viniciusgmenegon@gmail.com>
2021-08-09 15:58:17 -0300

    feat(server): add URI querys parameters to scope

--- server/src/presentation/helper/requireParams.ts
+++ server/src/presentation/helper/requireParams.ts
@@ -3,8 +3,13 @@ import { missingParamError } from "@/domain/err/helper"
 import { HttpRequest } from "../contracts/http"
 
 export function requiredParams(reqParans: string[], request: HttpRequest) {
+    const totalRequest = {
+        ...request.query,
+        ...request.body
+    }
+    
     for(const param of reqParans){
-        if(!request.body[param]){
+        if(!totalRequest[param]){
             return  new ErrorREST(missingParamError(param))
            
         }
