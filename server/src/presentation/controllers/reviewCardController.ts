773743ccbf59ddb6f0b586e9e2359b9c0ec82339
ViniciusGregorine <viniciusgmenegon@gmail.com>
2021-08-09 15:59:17 -0300

    feat(server): add requireParams to `addCard` and `reviewCard` use cases

--- server/src/presentation/controllers/reviewCardController.ts
+++ server/src/presentation/controllers/reviewCardController.ts
@@ -1,8 +1,8 @@
 
-import { IReview } from "@/domain/entities/review";
 import { IReviewCardUseCase } from "@/domain/usecases/reviewCard";
 import { IController } from "../contracts/controller";
 import { errorHandler, HttpRequest, HttpResponse, noContent } from "../contracts/http";
+import { requiredParams } from "../helper/requireParams";
 import { IReviewPresentationViewModel } from "../view-models/reviewPresentationViewModel";
 
 export class reviewCardController implements IController {
@@ -12,6 +12,12 @@ export class reviewCardController implements IController {
 
 
     async handle(request: HttpRequest<IReviewPresentationViewModel>): Promise<HttpResponse>{
+        const error = requiredParams(['userGrade', 'cod'], request)
+        console.log(error)
+        if (error) {
+            return errorHandler(error.response) 
+        }
+        
         const ReviewCard: IReviewPresentationViewModel = {
             cod: request.query!.cod,
             userGrade: request.body!.userGrade
