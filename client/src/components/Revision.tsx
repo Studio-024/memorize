3ca40b2c283aae9d92c6cbd68707b711158fe96c
k1vz <mrnerd777@gmail.com>
2021-08-13 17:31:49 -0300

    fix(client): create a function to change index and refactor style

--- client/src/components/Revision.tsx
+++ client/src/components/Revision.tsx
@@ -1,7 +1,7 @@
-import { useState, useEffect } from 'react'
 import '../css/Revision.css'
-import { ICardOrdered } from '../domain/useCase/orderCard'
+import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
+import { ICardOrdered } from '../domain/useCase/orderCard'
 
 export interface Props {
     dataCards: ICardOrdered[]
@@ -9,61 +9,65 @@ export interface Props {
 
 const Revision = ({dataCards}: Props) => {
     const [index, setIndex] = useState(0)
-
-    const [card, setCard] = useState<ICardOrdered[]>([{front: "null", back: "null"}])
-    const [route, setRoute] = useState("/dashboard/front")
+    const [card, setCard] = useState<ICardOrdered[]>([{front: 'null', back: 'null'}])
+    const [route, setRoute] = useState('/dashboard/front')
     
     useEffect(() => { 
-        if(dataCards.length !== 0){
+        if (dataCards.length !== 0) {
             setCard(dataCards)
         }
-        console.log("dataCards", dataCards)
      
     }, [dataCards])
 
-
-
-
     const switchRoute = () => {
-        if(route == "/dashboard/front"){
-            setRoute("/dashboard/back");
+        switch (route) {
+            case '/dashboard/front':
+                setRoute('/dashboard/back')
+                break
+            case '/dashboard/back':
+                setRoute('/dashboard/front')
+                break
         }
-        if(route == "/dashboard/back"){
-            setRoute("/dashboard/front");
-        }
-        
     }
-    return(
+
+    const nextCard = () => {
+        if (index+1 < card.length) { 
+            setIndex(index + 1)
+        } else {
+            card[index].front = 'Não há mais cards'
+            card[index].back = 'Não há mais cards'
+            switchRoute()
+        }
+    }
+
+    return (
         <>
-        <div className="card">
-            <h1 className="card_title">Title</h1>
-            <p className="card_quest">{
-                route === "/dashboard/front" 
+        <div className='card'>
+            <h1 className='card_title'>Title</h1>
+            
+            <p className='card_quest'>{
+                route === '/dashboard/front'
                     ? card[index].front 
                     : card[index].back
             }</p>
-            <div className="card_footer">
+
+            <div className='card_footer'>
                 <span>Total:&nbsp;<a>{index+1}/{card.length}</a></span> 
-                <Link to={route} onClick={switchRoute}><div> Virar Card</div></Link>
+                <Link to={route} onClick={switchRoute}><div>Virar Card</div></Link>
             </div>
         </div>
-            <div className="card_missAndHit">
-            <Link to="/dashboard/front">
-                <button onClick={() => {
-                    if (card.length != 0 && index+1 < card.length) { 
-                        setIndex(index + 1)
-                    } 
-                }} id="card_missed">Errei</button>
+
+        <div className='card_missAndHit'>
+            <Link to='/dashboard/front'>
+                <button id='card_missed' onClick={nextCard}>Errei</button>
             </Link>
 
-            <Link to="/dashboard/front"><button id="card_hit" onClick={() => {
-                if (card.length != 0 && index+1 < card.length) { 
-                    setIndex(index + 1)
-                } 
-            }}>Acertei</button></Link>
+            <Link to='/dashboard/front'>
+                <button id='card_hit' onClick={nextCard}>Acertei</button>
+            </Link>
             
         </div>
         </>
     )
 }
-export default Revision;
+export default Revision
--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,17 +1,14 @@
-import { useEffect, useState } from 'react'
-import RevisionFront from '../components/Revision'
-import RevisionBack from '../components/Revision'
 import { getCard } from '../service/api'
+import { useEffect, useState } from 'react'
+import Revision from '../components/Revision'
+import { ToastContainer } from 'react-toastify'
+import { Route, Switch } from 'react-router-dom'
+import 'react-toastify/dist/ReactToastify.min.css'
 import { ICardOrdered } from '../domain/useCase/orderCard'
 import { OrderCardService } from '../service/OrderCardService'
-import { ToastContainer } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.min.css'
-import { Route, Switch } from 'react-router-dom'
-
 
 const Dashboard = () => { 
     const [orderCards, setOrderCards ] = useState<ICardOrdered[]>([])
-
 
     async function downloadCards() {
         const orderObj = new OrderCardService()
@@ -23,20 +20,22 @@ const Dashboard = () => {
     useEffect(() => {
         downloadCards()
     }, [])
-    
+
 	return(
-        <div className="container" id="containerCard">
+        <div className='container' id='containerCard'>
             <main>
                 <ToastContainer />
-                <Switch>     
+                <Switch>
                     <Route exact path={`/dashboard`}>
-                        <RevisionFront dataCards={orderCards} />
-                    </Route>           
+                        <Revision dataCards={orderCards} />
+                    </Route>
                     <Route exact path={`/dashboard/front`}>
-                        <RevisionFront dataCards={orderCards}/>
+                        {/* Front */}
+                        <Revision dataCards={orderCards}/>
                     </Route>
                     <Route exact path={`/dashboard/back`}>
-                        <RevisionBack dataCards={orderCards}/>
+                        {/* Back */}
+                        <Revision dataCards={orderCards}/>
                     </Route>
                 </Switch>
             </main>
