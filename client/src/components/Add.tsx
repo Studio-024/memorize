5203f1172d8f56ef03ead74ee8810dd9810174cd
Alexandre Costa Belettini <costaalexandre45@gmail.com>
2021-08-18 21:05:08 -0300

    feat(client): add Card complete

--- client/src/components/Add.tsx
+++ client/src/components/Add.tsx
@@ -1,38 +1,39 @@
-import '../css/AddCard.css'
-import { saveCard } from '../service/api'
-import { ErrorHandler } from '../utils/ErrorHandler'
-import backSVG from  '../assets/back.svg'
-import { useHistory } from 'react-router'
+import '../css/AddCard.css';
+import { saveCard } from '../service/api';
+import { ErrorHandler } from '../utils/ErrorHandler';
+import backSVG from  '../assets/back.svg';
 
 const Add = () => {
-    let history = useHistory();
     function handleGoBack(){
-        history.goBack();
+        document.getElementById('AddContainer')!.style.display = 'none';
     }
     function create() {
-        const front = document.getElementById('front')!.innerHTML
-        const back = document.getElementById('back')!.innerHTML
+        const front = document.getElementById('front')!.innerHTML;
+        const back = document.getElementById('back')!.innerHTML;
 
         if (front && back) {
             saveCard(front, back)
-            document.getElementById('front')!.innerHTML = ''
-            document.getElementById('back')!.innerHTML = ''
+            document.getElementById('front')!.innerHTML = '';
+            document.getElementById('back')!.innerHTML = '';
         } else {
-            new ErrorHandler(422).Post()
+            new ErrorHandler(422).Post();
         }
     }
 
     return(
-        <>
-        <div id="AddBackground" onClick={handleGoBack}/>
-        <div className="AddCard">
-            <div id="AddBack" onClick={handleGoBack}><img src={backSVG} alt="" /></div><h1>Novo Card</h1>
-                <p contentEditable="true" spellCheck={true} className="AddCard_TextInput" placeholder="Titulo"></p>
+        <div id="AddContainer" style={{display:'none'}}>
+            <div id="AddBackground" onClick={handleGoBack}/>
+            <div className="AddCard">
+                <div id="AddBack" onClick={handleGoBack}>
+                    <img src={backSVG} alt="" />
+                </div>
+                <h1>Novo Card</h1>
+                <p contentEditable="true" spellCheck={true} className="AddCard_TextInput" placeholder={"Titulo"}></p>
                 <p contentEditable="true" spellCheck={true} className="AddCard_TextInput" placeholder={"Pergunta"}></p>
-                <p contentEditable="true" spellCheck={true} className="AddCard_TextInput" placeholder={"Resposta"}></p>
+                <p contentEditable="true" spellCheck={true} className="AddCard_TextInput" placeholder={"Resposta"}></p>                    
                 <button id='AddCreateCard_button'>Criar Card</button>
+            </div>
         </div>
-        </>
     )
 }
 export default Add;
--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,34 +1,41 @@
-import { getCard } from '../service/api'
-import { useEffect, useState } from 'react'
-import Revision from '../components/Revision'
-import { ToastContainer } from 'react-toastify'
-import { Route, Switch } from 'react-router-dom'
-import 'react-toastify/dist/ReactToastify.min.css'
-import { ICardOrdered } from '../domain/useCase/orderCard'
-import { OrderCardService } from '../service/OrderCardService'
-import AddButton from '../components/AddButton'
+import { getCard } from '../service/api';
+import { useEffect, useState } from 'react';
+import Revision from '../components/Revision';
+import { ToastContainer } from 'react-toastify';
+import { Link, Route, Switch, useHistory } from 'react-router-dom';
+import 'react-toastify/dist/ReactToastify.min.css';
+import { ICardOrdered } from '../domain/useCase/orderCard';
+import { OrderCardService } from '../service/OrderCardService';
+import '../css/AddButton.css';
+import Add from '../components/Add';
 
 const Dashboard = () => { 
-    const [orderCards, setOrderCards ] = useState<ICardOrdered[]>([])
+    let history = useHistory();
+    const [orderCards, setOrderCards ] = useState<ICardOrdered[]>([]);
 
+    function handleClickAdd(){
+        document.getElementById('AddContainer')!.style.display = 'initial';
+    }
     async function downloadCards() {
-        const orderObj = new OrderCardService()
-        const data = await orderObj.order(getCard())
+        const orderObj = new OrderCardService();
+        const data = await orderObj.order(getCard());
 
-        setOrderCards(data)
+        setOrderCards(data);
     }
 
     useEffect(() => {
-        downloadCards()
+        downloadCards();
     }, [])
 
 	return(
         <div className='container' id='containerCard'>
             <main>
                 <ToastContainer />
+                <Add/>
                 <Switch>
                     <Route exact path={`/dashboard`}>
-                        <Revision dataCards={orderCards} />
+                        <>{history.push('/')}</>
+                        <>{history.push('dashboard/front')}</>
                     </Route>
                     <Route exact path={`/dashboard/front`}>
                         {/* Front */}
@@ -39,10 +46,10 @@ const Dashboard = () => {
                         <Revision dataCards={orderCards}/>
                     </Route>
                 </Switch>
-                <AddButton/>
+                <button className="crud_button" onClick={handleClickAdd}>+</button>
             </main>
         </div>
 	)
 }
 
-export default Dashboard
+export default Dashboard;
