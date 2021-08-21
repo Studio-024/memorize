35573a214fd119918c4e6a80c33d042bb46b86f0
Alexandre Costa Belettini <costaalexandre45@gmail.com>
2021-08-21 18:31:21 -0300

    style(client): add class and ids in Registry.tsx

--- client/src/pages/Registry.tsx
+++ client/src/pages/Registry.tsx
@@ -1,15 +1,23 @@
+import '../css/Registry.css';
 
-const Registry = () =>{
+const Registry = () => {
   
   
   
   return(
     
     <>
-
-    <header>MEMORIZE</header>
-    <main></main>   
-    
+    <div className="registry">
+      <header className="registry_header">MEMORIZE</header>
+      <main className="registry_main">
+        <form className='registry_main_container_input'>
+          <input type="name" className="registry_main_input" placeholder="Nome"/>
+          <input type="email" className="registry_main_input" placeholder="Email"/>
+          <input type="password" className="registry_main_input" placeholder="Senha"/>
+          <button className="registry_main_submit">Criar Conta</button>
+        </form>
+      </main>   
+    </div>
     </>
 
   )
