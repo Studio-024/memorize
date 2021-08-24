7847c69ad8783332225d19171aab1a24077398ae
ViniciusGregorine <viniciusgmenegon@gmail.com>
2021-08-24 13:39:11 -0300

    feat(client): get input fields using react states

--- client/src/pages/Registry.tsx
+++ client/src/pages/Registry.tsx
@@ -1,26 +1,43 @@
+import { useState } from 'react';
 import '../css/Registry.css';
 
 const Registry = () => {
-  
-  
+  const [name, setName] = useState('')
+  const [password, setPassWord] = useState('')
+  const [email, setEmail] = useState('')
+
+ const  HandleSubmit = () => {
+   // hadle to-do
+  const userSbmit = {
+    name,
+    email,
+    password
+  }
+}
   
   return(
-    
     <>
     <div className="registry">
       <div id='registry_background'/>
       <header className="registry_header">MEMORIZE</header>
       <main className="registry_main">
-        <form className='registry_main_container_input'>
-          <input type="name" className="registry_main_input" placeholder="Nome"/>
-          <input type="email" className="registry_main_input" placeholder="Email"/>
-          <input type="password" className="registry_main_input" placeholder="Senha"/>
-          <button className="registry_main_submit">Criar Conta</button>
+        <form className='registry_main_container_input' >
+          <input type="name" className="registry_main_input" placeholder="Nome" 
+            onChange={event => setName(event.target.value)} 
+          />
+          <input type="email" className="registry_main_input" placeholder="Email"
+            onChange={event => setEmail(event.target.value)}
+          />
+          <input type="password" className="registry_main_input" placeholder="Senha"
+            onChange={event => setPassWord(event.target.value)}
+          />
+          <button className="registry_main_submit" onClick={HandleSubmit}>
+            Criar Conta
+          </button>
         </form>
       </main>
     </div>
     </>
-
   )
 }
 
