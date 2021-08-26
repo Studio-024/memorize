2f19476307b42f69906079c6d99b40c4ef488361
Alexandre Costa Belettini <costaalexandre45@gmail.com>
2021-08-26 15:30:19 -0300

    style(client): add Class And IDs in HTML Login

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,20 +1,20 @@
 import '../css/Login.css'
 
-
 const Login = () => {
 
   return(
-
     <div>
-      <header>MEMORIZE</header>
-      <main>
-       <form>
-          <input type="email" placeholder="Email"/>
-         <input type="password" placeholder="Senha"/>
-         <button>Entrar</button>
-         <a href="#">Esqueci a senha</a>
+      <div id="login_background"/>
+      <header className="login_header">
+        <h1 className="login_header_title">MEMORIZE</h1>
+      </header>
+      <main className="login_main">
+       <form className="login_main_form">
+          <input type="email" placeholder="Email" className="login_main_form_input"/>
+          <input type="password" placeholder="Senha" className="login_main_form_input"/>
+          <button className="login_main_form_submit">Entrar</button>
         </form>
-       <span>Criar Conta</span>
+       <span className="login_main_createAccont">Criar Conta</span>
       </main>
     </div>
 
