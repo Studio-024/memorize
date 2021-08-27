17f57d7db78fa4f07b7732c02466fd66f78e6c94
Alexandre Costa Belettini <costaalexandre45@gmail.com>
2021-08-27 16:23:16 -0300

    feat(client): complete Screen Login

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -3,18 +3,16 @@ import '../css/Login.css'
 const Login = () => {
 
   return(
-    <div>
+    <div className="login">
       <div id="login_background"/>
-      <header className="login_header">
-        <h1 className="login_header_title">MEMORIZE</h1>
-      </header>
+      <header className="login_header">MEMORIZE</header>
       <main className="login_main">
        <form className="login_main_form">
           <input type="email" placeholder="Email" className="login_main_form_input"/>
           <input type="password" placeholder="Senha" className="login_main_form_input"/>
           <button className="login_main_form_submit">Entrar</button>
         </form>
-       <span className="login_main_createAccont">Criar Conta</span>
+       <span id="login_main_createAccont">Criar Conta</span>
       </main>
     </div>
 
