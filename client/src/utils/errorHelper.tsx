1965ddf04acb65adddf253bc36de295bc7e0d5de
ViniciusGregorine <viniciusgmenegon@gmail.com>
2021-09-10 18:34:28 -0300

    feat(client): add `ErrorHandler` and  `errorHelper`

--- client/src/utils/errorHelper.tsx
+++ client/src/utils/errorHelper.tsx
@@ -2,7 +2,27 @@ import { toast } from "react-toastify"
 
 export const errorHelper = {
     sucess: (massege: string)=> toast.success(massege),
-    missingParameter: (parameter: string) => toast.warn(`campo: ${parameter} não preenchido`),
+    missingParameter: (parameter: string) => toast.warn(`${parameter} não preenchido`),
     emailAlredyExists: () => toast.warn('email já existe'),
-    apiError: (errorMassege: any) => {toast.warning(`${errorMassege}`)}
+
+    apiError: (errorStatusCode: number | null) => {
+        if (!errorStatusCode) toast.error('API off!')     
+        switch(errorStatusCode) {
+            case 400: 
+                toast.warn('bad request')
+                break
+            case 404: 
+                toast.error('Wrong URL/route!')
+                break
+            case 422:
+                toast.error('Missing parameter!')
+                break
+            case 500:
+                toast.error('DataBase off!')
+                break
+            default:
+                toast.error('Unexpected error!')
+                
+        }
+    }
 }
