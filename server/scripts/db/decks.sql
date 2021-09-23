1b76499fee10b69c6661de43724d36372ae4279e
ViniciusGregorine <viniciusgmenegon@gmail.com>
2021-09-23 18:24:15 -0300

    refactor: remove useless sql script

--- server/scripts/db/decks.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE decks (
-	cod INT NOT NULL AUTO_INCREMENT,
-	deck VARCHAR(30) NOT NULL,
-	category_cod INT,
-	PRIMARY KEY ( cod ),
-	FOREIGN KEY ( category_cod ) REFERENCES categories(cod)
-);
